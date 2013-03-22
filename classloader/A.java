72781080cadd5eb803c3dfe1bbf37d9d74cdc80f
brandom-iava <wujinwei10752@163.com>
2013-03-21 23:25:47 -0700

    iava

---
+++ classloader/A.java
@@ -0,0 +1,13 @@
+package com.iava.base.classloader;
+
+public class A {
+    private B b;
+ 
+    public void setB(B b) {
+         this .b = b;
+    }
+ 
+    public B getB() {
+         return b;
+    }
+}
